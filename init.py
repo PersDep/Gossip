33959049dc4d49888c89a8df8bbe7b7247844f81
Persdep <ilyanapar@yandex.ru>
2017-12-18 22:37:02 +0300

    Params update and run file

--- init.py
+++ init.py
@@ -3,17 +3,18 @@
 import subprocess as subproc
 import matplotlib.pyplot as pyplot
 import numpy as np
+import sys
 
-probabilities = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]#, 0.6]
+probabilities = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
 ticks = []
 
-iters = 4
+iters = 20
 
-nodes_amount = 10
-port = 4400
-min_degree = 2
-max_degree = 5
-ttl = 40
+nodes_amount = "40"
+port = "4400"
+min_degree = "2"
+max_degree = "8"
+ttl = "40"
 
 def plot(figsize, color, width):
     axis = np.zeros(len(probabilities))
@@ -24,7 +25,7 @@ def plot(figsize, color, width):
     pyplot.bar(axis, ticks, color=color, width=width)
     pyplot.xticks(axis + width / 2, probabilities)
 
-    pyplot.title(str(nodes_amount) + ' nodes_amount, ' + str(min_degree) + ' - ' + str(max_degree) + ' degree, ' + str(ttl) + ' TTL')
+    pyplot.title('Nodes: ' + nodes_amount + ', Node degree: ' + min_degree + ' - ' + max_degree + ', TTL: ' + ttl)
     pyplot.xlabel("Loss probability")
     pyplot.ylabel("Time (ticks)")
     pyplot.savefig("plot.jpg")
@@ -39,6 +40,12 @@ def bash_call(cmd):
         return error
     return result
 
+if len(sys.argv) == 6:
+    nodes_amount = sys.argv[1]
+    port = sys.argv[2]
+    min_degree = sys.argv[3]
+    max_degree = sys.argv[4]
+    ttl = sys.argv[5]
 
 for probability in probabilities:
     cur_ticks = 0.0
@@ -47,7 +54,7 @@ for probability in probabilities:
 
     cur_iter = iters
     while cur_iter > 0:
-        res = bash_call('./main ' + str(nodes_amount) + ' ' + str(port) + ' ' + str(min_degree) + ' ' + str(max_degree) + ' ' + str(ttl)).split()
+        res = bash_call('./main ' + nodes_amount + ' ' + port + ' ' + min_degree + ' ' + max_degree + ' ' + ttl).split()
         if res and res[0] == 'Finished':
             cur_ticks += float(res[2])
             cur_iter -= 1
